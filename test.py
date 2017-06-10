8c35c3599947957b9a714bf78c29d1ce4b681ce3
Rawn <rawnhenry@gmail.com>
2017-06-10 18:15:32 -0400

    new files

--- test.py
+++ test.py
@@ -1,68 +1,88 @@
 import tensorflow as tf
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
+from tensorflow.contrib.rnn import BasicLSTMCell, BasicRNNCell
+#from EURNN import EURNNCell
+
+def get_pixel_rep(n):   
+    
+    number_dict = {
+                    0 : np.array([[1,1,1], [1,0,1], [1,0,1], [1,0,1], [1,1,1]]),
+                    1 : np.array([[0,1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0]]),
+                    2 : np.array([[1,1,1], [0,0,1], [1,1,1], [1,0,0], [1,1,1]]),
+                    3 : np.array([[1,1,1], [0,0,1], [1,1,1], [0,0,1], [1,1,1]]),
+                    4 : np.array([[1,0,1], [1,0,1], [1,1,1], [0,0,1], [0,0,1]]),
+                    5 : np.array([[1,1,1], [1,0,0], [1,1,1], [0,0,1], [1,1,1]]),
+                    6 : np.array([[1,1,1], [1,0,0], [1,1,1], [1,0,1], [1,1,1]]),
+                    7 : np.array([[1,1,1], [0,0,1], [0,0,1], [0,0,1], [0,0,1]]),
+                    8 : np.array([[1,1,1], [1,0,1], [1,1,1], [1,0,1], [1,1,1]]),
+                    9 : np.array([[1,1,1], [1,0,1], [1,1,1], [0,0,1], [0,0,1]]),
+                   11 : np.array([[0,0,0], [0,1,0], [1,1,1], [0,1,0], [0,0,0]]), #plus sign
+                   12 : np.array([[0,0,0], [1,1,1], [0,0,0], [1,1,1], [0,0,0]])  #equal sign
+                  }
+    
+    def num_gen():
+        spacing = np.array([ [0] for i in range(5) ])    
+        symbols = {'+' : 11, '=' : 12}
+        
+        for char in str(n):
+            if char in symbols:
+                yield number_dict[symbols.get(char)]
+            else:
+                yield number_dict[int(char)]
+            
+            yield spacing
+     
+    return np.concatenate(list(num_gen()), axis = 1)
+
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return random.randint(range_start, range_end)
 
 def generate_data(num_of_points):
+ 
+    pluses = np.array([ get_pixel_rep("+") ]  * num_of_points)
+    equals = np.array([ get_pixel_rep("=") ] * num_of_points)
 
-	number_dict = {
-					0 : np.array([[1,1,1], [1,0,1], [1,0,1], [1,0,1], [1,1,1]]),
-					1 : np.array([[0,1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0]]),
-					2 : np.array([[1,1,1], [0,0,1], [1,1,1], [1,0,0], [1,1,1]]),
-					3 : np.array([[1,1,1], [0,0,1], [1,1,1], [0,0,1], [1,1,1]]),
-					4 : np.array([[1,0,1], [1,0,1], [1,1,1], [0,0,1], [0,0,1]]),
-					5 : np.array([[1,1,1], [1,0,0], [1,1,1], [0,0,1], [1,1,1]]),
-					6 : np.array([[1,1,1], [1,0,0], [1,1,1], [1,0,1], [1,1,1]]),
-					7 : np.array([[1,1,1], [0,0,1], [0,0,1], [0,0,1], [0,0,1]]),
-					8 : np.array([[1,1,1], [1,0,1], [1,1,1], [1,0,1], [1,1,1]]),
-					9 : np.array([[1,1,1], [1,0,1], [1,1,1], [0,0,1], [0,0,1]]),
-				   11 : np.array([[0,0,0], [0,1,0], [1,1,1], [0,1,0], [0,0,0]]), #plus sign
-				   12 : np.array([[0,0,0], [1,1,1], [0,0,0], [1,1,1], [0,0,0]])  #equal sign
-				  }
+    first_reps = []
+    sec_reps   = []
+    labels     = []
 
-	num_digits = 3
+    for i in range(num_of_points):
+        first_num = random_with_N_digits(3)
+        first_rep = get_pixel_rep(first_num)
+        sec_num   = random_with_N_digits(3)
+        sec_rep   = get_pixel_rep(sec_num)
 
-	pluses = np.array([number_dict[11]] * num_of_points )
-	equals = np.array([number_dict[12]] * num_of_points)
+        labels.append(get_pixel_rep(first_num + sec_num))
+        first_reps.append(first_rep)
+        sec_reps.append(sec_rep)
 
+    labels     = np.array(labels)
+    first_reps = np.array(first_reps)
+    sec_reps   = np.array(sec_reps)
 
-	first_reps = []
-	sec_reps   = []
-	labels     = []
+    data = np.concatenate((first_reps, pluses, sec_reps, equals), axis = 2)
+    
 
-	for i in range(num_of_points):
-		first_num, first_rep = make_n_digit_number(num_digits, number_dict)
-		sec_num,   sec_rep   = make_n_digit_number(num_digits, number_dict)
-
-		labels.append(first_num + sec_num)
-		first_reps.append(first_rep)
-		sec_reps.append(sec_rep)
-
-	labels     = np.array(labels)
-	first_reps = np.array(first_reps)
-	sec_reps   = np.array(sec_reps)
-
-	print(first_reps)
-	data = np.concatenate((first_reps, pluses, sec_reps), axis = 2)
-
-	return data, labels
+    return data, labels
 
 
-def make_n_digit_number(n, number_dict):
-	power = n - 1
-	number = 0
-	seq = []
+#Set up hyper-params
+learning_rate = 0.01
+epochs        = 10000
+batch_size    = 128
+ 
+#Set up neural-net parameters
+n_input   = 5    #rows in image
+n_steps   = 32   #columns to read in image
+n_hidden  = 128  #hidden neurons
+n_classes = 1998 #numbered output of NN 
 
-	while(power >= 0):
-		if power == n - 1:
-			digit = random.randint(1,9)
-		else:
-			digit = random.randint(0,9)
 
-		number += digit * 10**power
-		power -= 1
-		seq.append(number_dict[digit])
-
-	return number, np.concatenate(seq, axis = 1)
 
 
